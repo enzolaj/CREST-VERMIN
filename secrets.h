9a1408530555640ee3dc3ad555b7093b0fc39f2e
Kai-Xing Yue <79027796+Fantasticalkiwi@users.noreply.github.com>
2025-08-11 12:21:09 -0400

    NTRIP Client code
    Uploading rover files for configuring ESP32 for NTRIP

---
+++ secrets.h
@@ -0,0 +1,18 @@
+//Your WiFi credentials
+const char *ssid = "OLIN-DEVICES";
+const char *password = "Engineering4Every1!";
+
+//RTK2Go works well and is free
+//const char casterHost[] = "rtk2go.com";
+//const uint16_t casterPort = 2101;
+//const char mountPoint[] = "bldr_dwntwn2"; //The mount point you want to push data to
+//const char mountPointPW[] = "WR5wRo4H";
+
+//Emlid Caster also works well and is free
+const char casterHost[] = "caster.emlid.com";
+const uint16_t casterPort = 2101;
+const char casterUser[] = "u29226";
+const char casterUserPW[] = "459yqs";
+const char mountPoint[] = "MP23544"; //The mount point you want to push data to
+const char mountPointPW[] = "287xrm";
+
